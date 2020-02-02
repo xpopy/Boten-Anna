13bf49a817b7f31f319a229775f5d6c4d135bfe8
xpopy <oskardahlin10@hotmail.com>
2020-02-02 22:10:06 +0100

    Test for portable git

--- main.py
+++ main.py
@@ -25,27 +25,39 @@ if __name__ == "__main__":
 
 	if exitCode == "update":
 
+		print("\nUpdating...\n")
+
+		gitDir = "./bin/git/cmd/git.exe"
+
 		if not os.path.isdir('.git'):
 			raise EnvironmentError("Missing .git folder.")
 
 		# Make sure that we can actually use Git on the command line
 		try:
-			subprocess.check_call('git --version', shell=True, stdout=subprocess.DEVNULL)
+			with open('tmpFile', "w") as outfile:
+				subprocess.check_call(f'{gitDir} --version', stdout=outfile)
+			if os.path.exists("tmpFile"):
+				os.remove("tmpFile")
 		except subprocess.CalledProcessError:
+			if os.path.exists("tmpFile"):
+				os.remove("tmpFile")
 			raise EnvironmentError("Couldn't use Git on the CLI. You will need to run 'git pull' yourself.")
-
-		print("Passed Git checks...")
-
+			
 		#Ignoring changes done to the custom.py file so we don't overwrite it
-		subprocess.check_call('git update-index --assume-unchanged cogs/custom.py', shell=True)
+		subprocess.check_call(f'{gitDir} update-index --assume-unchanged cogs/custom.py')
 
 		try:
-			subprocess.check_call('git pull', shell=True)
+			out = subprocess.check_output(f'{gitDir} pull')
+			text = out.decode("utf-8")
+			print(text)
+
+			if not "Already up to date" in text:
+				print("\nUpdate successful! Loading...\n")
+
 		except subprocess.CalledProcessError:
 			raise OSError("Could not update the bot. You have modified files that are tracked by Git (e.g the bot\'s source files).\n" +
 							"You will need to run 'git pull' yourself or manually update the files.")
 
-		print("Update successful, restarting...")
 		subprocess.call([sys.executable, "main.py"])
 
 
