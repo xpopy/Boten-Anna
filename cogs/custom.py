33cbf502e4492dbdf1134c7cfa35c82e7f095ee9
Oskar Dahlin <oskardahlin10@hotmail.com>
2020-04-03 15:16:15 +0200

    Update custom.py

--- cogs/custom.py
+++ cogs/custom.py
@@ -1,17 +1,20 @@
 import discord
 from discord.ext import commands
+import sys
 
+sys.path.append('./cogs')
+import utils
 
 class Custom(commands.Cog):
 
 	def __init__(self, bot):
 		self.bot = bot
 
-	#This is an example command, change the "exampleFunction" to whatever you want your command to b, or add a completely new function
+	#This is an example command, change the "wave" to whatever you want your command to be, or add a completely new function
 	@commands.command()
-	async def exampleFunction(self,  ctx, *, user: discord.Member):
+	async def wave(self, ctx, *, user: discord.Member):
 		#Command code goes here
-		await ctx.send(f"Hello {user.mention}")
+		await utils.tenorAPI(ctx, "anime wave", f"**{ctx.author.mention} waves to {user.mention}**")
 
 
 def setup(bot):
